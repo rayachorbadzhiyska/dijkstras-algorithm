f31450f751be369d4af1611806b745a62356deb0
Elliiie <eli.georgieva.06@gmail.com>
2023-02-07 22:53:01 +0200

    Improve comments

--- HeapNode.h
+++ HeapNode.h
@@ -1,22 +1,40 @@
 #ifndef HEAPNODE_H
 #define HEAPNODE_H
 
-/// Represents a node from the Min-Heap
+/*!
+ * Represents a node from the Min-Heap
+ */
 class HeapNode
 {
 public:
-    /// Default constructor
+    /*!
+     * Default constructor
+     */
     HeapNode();
-    /// Constructor - creates a node using the specified value and cost
+    /*!
+     * Constructor - creates a node using the specified value and cost
+     * \param value The node's value
+     * \param cost The node's cost
+     */
     HeapNode(int value, int cost);
 
-    /// Returns the value of a node
+    /*!
+     * Returns the value of the node
+     */
     int getValue() const;
-    /// Updates the value of a node
+    /*!
+     * Updates the value of a node
+     * \param newValue The new value
+     */
     void setValue(int newValue);
-    /// Returns the cost of a node
+    /*!
+     * Returns the cost of the node
+     */
     int getCost() const;
-    /// Updates the cost of a node
+    /*!
+     * Updates the cost of a node
+     * \param newValue The new cost
+     */
     void setCost(int newValue);
 
 private:
