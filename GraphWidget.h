1264cce977a02e93047faccfd02e365454b0b42d
Ilia Parvanov <ilia.nikolaev.parvanov@gmail.com>
2023-02-07 23:04:26 +0200

    Comment in GraphWidget.h

--- GraphWidget.h
+++ GraphWidget.h
@@ -15,34 +15,78 @@ class GraphWidget : public QWidget
 public:
     GraphWidget(QWidget *parent);
     ~GraphWidget();
+
+    /*! Set which graph the widget will visualize
+     *
+     * \param graph The graph
+     */
     void setGraph(Graph *graph);
 
     // Function used to schedule a timer to draw a path that had been processed in the algorithm
     void scheduleTimerForDrawingPath(int source, int destination);
 
 public slots:
-    // Visualize the graph by repainting it
+    /*! Visualize the graph by repainting it
+     *
+     */
     void visualize();
-    // Redraws the graph and highlights a node
+
+    /*! Redraws the graph and highlights a node
+     *
+     * \param nodeValue Node to be highlighted
+     */
     void highlightNode(int nodeValue);
-    // Redraws the graph and highlights an edge
+
+    /*! Redraws the graph and highlights an edge
+     *
+     * \param source Source node of edge
+     * \param destination Destination node of edge
+     */
     void highlightEdge(int source, int destination);
-    // Redraws the graph and removes the highlight from node
+
+    /*! Redraws the graph and removes the highlight from node
+     *
+     * \param nodeValue Node to be unhighlighted
+     */
     void unHighlightNode(int nodeValue);
-    // Redraws the graph and removes the highlight from edge
+
+    /*! Redraws the graph and removes the highlight from edge
+     *
+     * \param source Source node of edge
+     * \param destination Destination node of edge
+     */
     void unHighlightEdge(int source, int destination);
-    // Redraws the graph and removes all highlights
+
+    /*! Redraws the graph and removes all highlights
+     *
+     */
     void unHighlightAll();
 
 protected:
-    // Called on update()
+    /*! Called on update()
+     *
+     * \param event The QPaintEvent
+     */
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    // Arranges all graph nodes on a single circle and returns coordinates of each node
+    /*!
+     * Arranges all graph nodes on a single circle and returns coordinates of each node
+     *
+     * \param event The paint event, needed to take use its center as anchor
+     * \return Vector of QPoints, each point being the center of a node
+     */
     std::vector< QPoint > calculateNodeCoordinates(QPaintEvent *event);
-    // Draws a single edge, consisting of a line and arrow head
-    // Returns line from source node border to dest node border
+
+    /*!
+     * Draws a single edge, consisting of a line and arrow head
+     * The visible line extends from the source node border to destination node border
+     *
+     * \param painter The painter used to draw
+     * \param sourceDestLine Line from source node center to destination node center
+     * \param sourceNodeRect Bounding rectangle of source node
+     * \return Line from source node border to dest node border
+     */
     QLineF drawEdge(QPainter *painter, QLine sourceDestLine, QRect sourceNodeRect);
 
     QBrush circleBrush;
@@ -55,7 +99,8 @@ private:
     std::set< int > highlightedNodes;
     std::set< std::pair< int, int > > highlightedEdges;
 
-    // An array of timers used to start a drawing process of path between source and destination; An index in the array corresponds to a node's value
+    // An array of timers used to start a drawing process of path between source and destination;
+    // An index in the array corresponds to a node's value
     std::vector<QTimer*> timers;
 
     const QSize NODE_RECT_SIZE = QSize(20, 20);
